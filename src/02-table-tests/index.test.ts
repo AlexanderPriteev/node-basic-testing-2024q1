d7408a5a3833c37bcf9f9b626b3d8b3a93b17db2
priteev <alexander.priteev@sibers.com>
2024-02-10 03:51:05 +0600

    feat: solution for 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,34 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+type TCase = {
+  a: number;
+  b: number;
+  action: Action;
+  expected: number;
+};
+
+const testCases: TCase[] = [
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  // continue cases for other actions
+  { a: 10, b: 10, action: Action.Subtract, expected: 0 },
+  { a: 10, b: -10, action: Action.Subtract, expected: 20 },
+  { a: 10, b: NaN, action: Action.Subtract, expected: NaN },
+  { a: 10, b: 10, action: Action.Multiply, expected: 100 },
+  { a: 10, b: -10, action: Action.Multiply, expected: -100 },
+  { a: 10, b: NaN, action: Action.Multiply, expected: NaN },
+  { a: 10, b: 10, action: Action.Divide, expected: 1 },
+  { a: 10, b: -10, action: Action.Divide, expected: -1 },
+  { a: 10, b: NaN, action: Action.Divide, expected: NaN },
+  { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
+  { a: 100, b: 0.5, action: Action.Exponentiate, expected: 10 },
+  { a: -10, b: 0.5, action: Action.Exponentiate, expected: NaN },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('should blah-blah', (item: TCase) => {
+    expect(simpleCalculator(item)).toBe(item.expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
